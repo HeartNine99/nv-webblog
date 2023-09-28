f32e942c7a515e2a0007d776864d32b6c33bcd9f
HeartNine99 <hearttnine@gmail.com>
2023-09-28 22:44:37 +0700

    27-9-2023

--- server/src/app.js
+++ server/src/app.js
@@ -1,43 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require('cors')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 require('./route')(app)
 
-app.get('/status', function (req, res) {
-    res.send('Hello nodejs server belonging to nitiphumi');
-});
+// app.get('/status', function (req, res) {
+//     res.send('Hello nodejs server belonging to nitiphumi');
+// });
 
-app.get('/hello/:name', function (req, res) {
-    console.log('hello - ' + req.params.name);
-    res.send('Say hello with ' + req.params.name);
-});
+// app.get('/hello/:name', function (req, res) {
+//     console.log('hello - ' + req.params.name);
+//     res.send('Say hello with ' + req.params.name);
+// });
 
-app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน' + req.params.userId);
-});
+// app.get('/user/:userId', function (req, res) {
+//     res.send('ดูข้อมูลผู้ใช้งาน' + req.params.userId);
+// });
 
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด');
-});
+// app.get('/users', function (req, res) {
+//     res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด');
+// });
 
-app.post('/user/:userId', function (req, res) {
-    res.send('ทำการสร้างผู้ใช้งาน ' + JSON.stringify(req.body));
-});
+// app.post('/user/:userId', function (req, res) {
+//     res.send('ทำการสร้างผู้ใช้งาน ' + JSON.stringify(req.body));
+// });
 
-app.put('/user/:userId', function (req, res) {
-    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
-});
+// app.put('/user/:userId', function (req, res) {
+//     res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+// });
 
-app.delete('/user/:userId', function (req, res) {
-    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body));
-});
+// app.delete('/user/:userId', function (req, res) {
+//     res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body));
+// });
 
-const port = 8081;
-app.listen(port, function () {
-    console.log('Server running on port ' + port);
-});
+let port = process.env.PORT || config.port;
+
+sequelize.sync({force: false}).then(() => {
+ app.listen(port, function () {
+ console.log('Server running on ' + port)
+ })
+})
+
